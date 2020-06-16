b13ff213b4802fa9b9b8086cf0ab6c00347802c0
Lesot Xavier <lesotxavier@gmail.com>
2020-06-16 16:00:57 +0200

    Feature complete: Dictionary check of attempt

--- game.py
+++ game.py
@@ -1,5 +1,6 @@
 import string
 import random
+import requests
 
 class Game:
     def __init__(self):
@@ -15,4 +16,11 @@ class Game:
                 letters.remove(letter)
             else:
                 return False
-        return True
+
+        return self.__check_dictionary(word)
+    def __check_dictionary(self, word):
+        r = requests.get(f"https://wagon-dictionary.herokuapp.com/{word}")
+        response = r.json()
+
+        print(r)
+        return response['found']
